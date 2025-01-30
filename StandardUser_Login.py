535770ae9d48ff44906e07098b80c81c3e71b127
ValentinnoCruz <valentinnocruz@gmail.com>
2025-01-29 16:52:27 -0800

    Updated Login method, Wait for elements to load prior to interactive with the elements

--- StandardUser_Login.py
+++ StandardUser_Login.py
@@ -18,16 +18,21 @@ class TestUserLogin():
         # Open the login page
         driver.get("https://sprout-qa-2-wioqjc6rsa-wl.a.run.app/#/login")
 
-        # Input email and password
-        driver.find_element(By.ID, "email").send_keys("QAAdmin3@canon.com")
-        driver.find_element(By.ID, "current-password").send_keys("Canon1234")
+        # Wait for the login page to load before interacting with the elements
+        wait = WebDriverWait(driver, 10) # wait for 10 seconds
+        email_input = wait.until(EC.presence_of_element_located((By.ID, "email"))) # wait for the email input field to load
+        email_input.send_keys("QAStandard4@canon.com")
+        
+        # Wait for the password input field to load
+        PW_input = wait.until(EC.presence_of_element_located((By.ID, "current-password")))
+        PW_input.send_keys("Canon1234")
 
         # Click on the Login Button
         driver.find_element(By.XPATH, "//button[normalize-space()='SIGN IN']").click()
 
         #Explicit wait for the 
         try:
-            WebDriverWait(driver, 3).until(
+            WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.XPATH, "//div[@class='user-role']"))
             )
             print("User logged in successfully")
