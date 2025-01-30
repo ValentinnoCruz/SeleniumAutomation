a64f5187c51506b86227ca225536713fbe5b8062
ValentinnoCruz <valentinnocruz@gmail.com>
2025-01-29 16:00:20 -0800

    Standard User Login - Initial Commit

---
+++ StandardUser_Login.py
@@ -0,0 +1,29 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+class TestUserLogin():
+    def test_User_Login(self):
+
+        # Initialize ChromeDriver
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+        # Open the login page
+        driver.get("https://sprout-qa-2-wioqjc6rsa-wl.a.run.app/#/login")
+
+        # Input email and password
+        driver.find_element(By.ID, "email").send_keys("QAAdmin3@canon.com")
+        driver.find_element(By.ID, "current-password").send_keys("Canon1234")
+
+        # Click on the Login Button
+        driver.find_element(By.XPATH, "//button[normalize-space()='SIGN IN']").click()
+
+        #Explicit wait for the "Scan Test Result" button
+
